53175344ebc05c1d9ead9f758e54336b2ac7fe55
Mitar <mitar.git@tnode.com>
2013-06-27 05:11:14 -0700

    Added dependency.

--- package.js
+++ package.js
@@ -2,6 +2,10 @@ Package.describe({
   summary: "Meteor smart package for AWS SDK node.js package"
 });
 
+Npm.depends({
+  'aws-sdk': "1.3.1"
+});
+
 Package.on_use(function (api) {
   api.add_files([
     'server.js'
