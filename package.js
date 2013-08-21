3ae1712c6b8b0f59bf1e1e025a71bd8c254a5a09
Mitar <mitar.git@tnode.com>
2013-08-21 20:20:28 -0300

    Updated for Meteor 0.6.5.

--- package.js
+++ package.js
@@ -7,6 +7,8 @@ Npm.depends({
 });
 
 Package.on_use(function (api) {
+  api.export('AWS');
+
   api.add_files([
     'server.js'
   ], 'server');
--- tests.js
+++ tests.js
@@ -7,4 +7,5 @@ Tinytest.add('meteor-aws-sdk', function (test) {
   catch (e) {
   }
   test.isTrue(isDefined, "AWS is not defined");
+  test.isTrue(Package['aws-sdk'].AWS, "Package.aws-sdk.AWS is not defined");
 });
