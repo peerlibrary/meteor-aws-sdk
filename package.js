4761de39efce96798e9e586d084986e02f793b7f
Mitar <mitar.git@tnode.com>
2014-09-12 22:22:37 -0700

    Transitory package.

--- package.js
+++ package.js
@@ -1,26 +1,10 @@
 Package.describe({
   summary: "SDK for AWS services including Amazon S3, Amazon EC2, DynamoDB, and Amazon SWF",
   version: '2.0.17-1',
-  name: 'peerlibrary:aws-sdk',
+  name: 'mrt:aws-sdk',
   git: 'https://github.com/peerlibrary/meteor-aws-sdk.git'
 });
 
-Npm.depends({
-  'aws-sdk': '2.0.17'
-});
-
 Package.on_use(function (api) {
-  api.versionsFrom('METEOR@0.9.1.1');
-  api.use('peerlibrary:blocking@0.4.2');
-
-  api.export('AWS');
-
-  api.add_files([
-    'server.js'
-  ], 'server');
-});
-
-Package.on_test(function (api) {
-  api.use(['peerlibrary:aws-sdk', 'tinytest', 'test-helpers'], 'server');
-  api.add_files('tests.js', 'server');
+  api.imply('peerlibrary:aws-sdk@2.0.17-1');
 });
