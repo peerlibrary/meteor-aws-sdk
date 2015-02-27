5b4722929781ea551411dcfaac0c2151f5d5358c
Mitar <mitar.git@tnode.com>
2015-02-26 22:08:10 -0800

    Version bump.

--- package.js
+++ package.js
@@ -1,17 +1,17 @@
 Package.describe({
   summary: "SDK for AWS services including Amazon S3, Amazon EC2, DynamoDB, and Amazon SWF",
-  version: '2.1.6_2',
+  version: '2.1.14_1',
   name: 'peerlibrary:aws-sdk',
   git: 'https://github.com/peerlibrary/meteor-aws-sdk.git'
 });
 
 Npm.depends({
-  'aws-sdk': '2.1.6'
+  'aws-sdk': '2.1.14'
 });
 
 Package.on_use(function (api) {
-  api.versionsFrom('METEOR@1.0.2.1');
-  api.use('peerlibrary:blocking@0.5.1');
+  api.versionsFrom('METEOR@1.0.3.2');
+  api.use('peerlibrary:blocking@0.5.2');
 
   api.export('AWS');
 
