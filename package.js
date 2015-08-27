7fbf03a3d8feb7cab678cb0ab571310967d3ee91
getninjaN <getninjan@gmail.com>
2015-08-27 15:59:25 +0200

    Update aws-sdk-js to 2.1.47

--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
   summary: "SDK for AWS services including Amazon S3, Amazon EC2, DynamoDB, and Amazon SWF",
-  version: '2.1.17_1',
+  version: '2.1.47_1',
   name: 'peerlibrary:aws-sdk',
   git: 'https://github.com/peerlibrary/meteor-aws-sdk.git'
 });
 
 Npm.depends({
-  'aws-sdk': '2.1.17'
+  'aws-sdk': '2.1.47'
 });
 
 Package.on_use(function (api) {
