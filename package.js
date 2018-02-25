74393a2cf82c1ee5ebed9643431403b92b72d96e
Mitar <mitar.git@tnode.com>
2018-02-25 02:22:41 -0800

    Added patch version.

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "SDK for AWS services including Amazon S3, Amazon EC2, DynamoDB, and Amazon SWF",
-  version: '2.200.0',
+  version: '2.200.0_1',
   name: 'peerlibrary:aws-sdk',
   git: 'https://github.com/peerlibrary/meteor-aws-sdk.git'
 });
