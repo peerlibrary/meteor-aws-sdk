f7d9da225717d865a5c8116f71eb2ef5e0a55b57
Mitar <mitar.git@tnode.com>
2019-09-19 18:59:19 -0700

    Version bump.

--- package.js
+++ package.js
@@ -1,32 +1,43 @@
 Package.describe({
   summary: "SDK for AWS services including Amazon S3, Amazon EC2, DynamoDB, and Amazon SWF",
-  version: '2.476.0_1',
+  version: '2.531.0_1',
   name: 'peerlibrary:aws-sdk',
   git: 'https://github.com/peerlibrary/meteor-aws-sdk.git'
 });
 
 Npm.depends({
-  'aws-sdk': '2.476.0'
+  'aws-sdk': '2.531.0'
 });
 
-Package.on_use(function (api) {
-  api.versionsFrom('METEOR@1.0.3.2');
-  api.use(['peerlibrary:blocking@0.5.2', 'underscore']);
+Package.onUse(function (api) {
+  api.versionsFrom('METEOR@1.8.1');
+
+  api.use([
+    'peerlibrary:blocking@0.6.0',
+    'underscore'
+  ]);
 
   api.export('AWS');
 
-  api.add_files([
+  api.addFiles([
     'server.js'
   ], 'server');
 
-  api.add_files([
+  api.addFiles([
     'aws-sdk-js/dist/aws-sdk.js',
     'client.js'
   ], 'client');
 });
 
-Package.on_test(function (api) {
-  api.use(['peerlibrary:aws-sdk', 'tinytest', 'test-helpers'], ['client', 'server']);
-  api.add_files('client-tests.js', 'client');
-  api.add_files('server-tests.js', 'server');
+Package.onTest(function (api) {
+  api.versionsFrom('METEOR@1.8.1');
+
+  api.use([
+    'peerlibrary:aws-sdk',
+    'tinytest',
+    'test-helpers'
+  ], ['client', 'server']);
+
+  api.addFiles('client-tests.js', 'client');
+  api.addFiles('server-tests.js', 'server');
 });
