80c883da59d0103799f4b2dcc6024f750afacff0
MrSpark2591 <mr.spark2591@gmail.com>
2019-12-17 16:50:38 +0530

    made for client side only

--- package.js
+++ package.js
@@ -1,8 +1,8 @@
 Package.describe({
   summary: "SDK for AWS services including Amazon S3, Amazon EC2, DynamoDB, and Amazon SWF",
   version: '2.531.0_1',
-  name: 'peerlibrary:aws-sdk',
-  git: 'https://github.com/peerlibrary/meteor-aws-sdk.git'
+  name: 'mrspark2591:aws-sdk',
+  git: 'https://github.com/Fasal-Tech/meteor-aws-sdk.git'
 });
 
 Npm.depends({
@@ -22,22 +22,16 @@ Package.onUse(function (api) {
   api.addFiles([
     'server.js'
   ], 'server');
-
-  api.addFiles([
-    'aws-sdk-js/dist/aws-sdk.js',
-    'client.js'
-  ], 'client');
 });
 
 Package.onTest(function (api) {
   api.versionsFrom('METEOR@1.8.1');
 
   api.use([
-    'peerlibrary:aws-sdk',
+    'mrspark2591:aws-sdk',
     'tinytest',
     'test-helpers'
-  ], ['client', 'server']);
-
-  api.addFiles('client-tests.js', 'client');
+  ], ['server']);
+  
   api.addFiles('server-tests.js', 'server');
 });
