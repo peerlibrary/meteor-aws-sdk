38a5cbe75cb12b7a51babc5c72e6706bf434bdf8
MrSpark2591 <mr.spark2591@gmail.com>
2019-12-21 16:58:58 +0530

    changes as per review

--- client-tests.js
+++ client-tests.js
@@ -1,3 +1,17 @@
-Tinytest.add('aws-sdk', function(test) {
-  test.isFalse(Package['peerlibrary:aws-sdk'].AWS, "Package.peerlibrary:aws-sdk.AWS is not defined");
+import { AWS } from 'meteor/peerlibrary:aws-sdk';
+
+Tinytest.add('aws-sdk', function (test) {
+  var isDefined = false;
+  try {
+    AWS;
+    isDefined = true;
+  }
+  catch (e) {
+  }
+  test.isTrue(isDefined, "AWS is not defined");
+  
+  var s3 = new AWS.S3();
+  test.isTrue(s3);
+  test.isTrue(s3.getObject);
+  test.isTrue(s3.upload);
 });
--- server-tests.js
+++ server-tests.js
@@ -1,7 +1,17 @@
-Tinytest.add('aws-sdk', function(test) {
+import { AWS } from 'meteor/peerlibrary:aws-sdk';
+
+Tinytest.add('aws-sdk', function (test) {
+  var isDefined = false;
+  try {
+    AWS;
+    isDefined = true;
+  }
+  catch (e) {
+  }
+  test.isTrue(isDefined, "AWS is not defined");
   test.isTrue(Package['peerlibrary:aws-sdk'].AWS, "Package.peerlibrary:aws-sdk.AWS is not defined");
 
-  var s3 = new Package['peerlibrary:aws-sdk'].AWS.S3();
+  var s3 = new AWS.S3();
   test.isTrue(s3);
   test.isTrue(s3.getObjectSync);
   test.isTrue(s3.getObjectSync._blocking);
